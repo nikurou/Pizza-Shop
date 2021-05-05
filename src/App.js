63dc10c0d3d9e36479b12acf6a1fa658213cc19e
nikurou <dylan.chung1997@gmail.com>
2021-05-05 00:12:33 -0700

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,49 +1,26 @@
 import "./App.css";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Button } from "@material-ui/core";
+
+import Footer from "./components/Footer";
+import Home from "./screens/Home";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <div>
-          <Link to="/">
-            <Button>Home</Button>
-          </Link>
-          <Link to="/about">
-            <Button>About</Button>
-          </Link>
-          <Link to="/users">
-            <Button>Users</Button>
-          </Link>
-        </div>
+        <Switch>
+          <Route path="/about">{/* <About /> */}</Route>
+          <Route path="/users">{/* <Users /> */}</Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
       </div>
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
     </Router>
   );
-}
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App;
