21de193e8ee29128a937794cedfade5757650f06
nikurou <dylan.chung1997@gmail.com>
2021-05-05 00:12:58 -0700

    Homepage skeleton added

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Colors from "../constants/Color";
+
+const useStyles = makeStyles((theme) => ({}));
+
+const Home = (props) => {
+  const classes = useStyles();
+  return <div></div>;
+};
+
+export default Home;
