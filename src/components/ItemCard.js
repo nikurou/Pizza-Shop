74782d347961a97419a9837dbff2c4fdfe8379a3
nikurou <dylan.chung1997@gmail.com>
2021-05-12 18:55:36 -0700

    itemCard finished

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,17 +2,43 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Colors from "../constants/Color";
 import tastemaker from "../assets/images/tastemaker.jpg";
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   cardContainer: {
     width: "30%",
     height: "315px",
     overflow: "hidden",
-    border: "1px solid lightgray ",
+    border: "1px solid gray ",
+    backgroundColor: Colors.backgroundSecondary,
+    overflow: "hidden",
+    paddingBottom: "1%",
   },
   cardImage: {
     width: "100%",
     height: "auto",
+  },
+  cardTitle: {
+    "& $h3": {
+      marginTop: "0px",
+    },
+  },
+  cardDescription: {},
+  infoContainer: {
+    color: "white",
+    marginLeft: "2%",
+    fontFamily: "'sharp_sans_bold',sans-serif",
+    marginTop: "5px",
+  },
+  cardButtonContainer: {
+    marginTop: "3%",
+  },
+  buttonRoot: {
+    backgroundColor: "black",
+    height: "40px",
+    color: "white",
+    "&:hover": { backgroundColor: "lightgray" },
+    borderRadius: "10px",
   },
 }));
 const ItemCard = (props) => {
@@ -23,9 +49,19 @@ const ItemCard = (props) => {
       <div className={classes.cardImage}>
         <img className={classes.cardImage} src={tastemaker} />
       </div>
-      <div className={classes.cardTitle}></div>
-      <div className={classes.cardDescription}> </div>
-      <div className={classes.cardButton}></div>
+      <div className={classes.infoContainer}>
+        <div className={classes.cardTitle}>
+          <h3>Lorem Ipsum dolor</h3>
+        </div>
+        <div className={classes.cardDescription}>
+          <span>SIT AMET, CONSECTETUR ADIPSCING ELIT.</span>
+        </div>
+        <div className={classes.cardButtonContainer}>
+          <Button classes={{ root: classes.buttonRoot }}>
+            <h5>Order Now</h5>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
