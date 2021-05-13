6699b20097e69a6d50a3dbbe2e6f6483ba1212f8
nikurou <dylan.chung1997@gmail.com>
2021-05-13 10:11:45 -0700

    Updates

--- src/constants/Color.js
+++
@@ -1,8 +0,0 @@
-export default {
-  primaryColor: "#fffff",
-  accentColor: "#ff6f00",
-  blackText: "#131313",
-  redText: "#e71316",
-  backgroundPrimary: "rgb(15,15,15)",
-  backgroundSecondary: "#e71316",
-};
