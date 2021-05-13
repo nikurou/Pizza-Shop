ec3195f88c42bd990ff54e77a0aa60e969cd8f5f
nikurou <dylan.chung1997@gmail.com>
2021-05-13 00:38:50 -0700

    expand on hover functionality

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,23 +1,41 @@
 import React from "react";
-import menu_image from "../assets/images/menu_image.jpg";
+
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   menuContainer: {
-    backgroundImage: `url(${menu_image})`,
     height: 141,
     width: "30%",
     marginTop: "2%",
+    direction: "ltr",
+    display: "inline-block",
+    position: "relative",
+
+    "& img": {
+      height: 141,
+      transition: "width 1s, height 1s",
+
+      width: "100%",
+    },
+
+    "& :hover": {
+      width: "110%",
+      height: "110%",
+    },
   },
   menuImage: {
     width: "20%",
   },
   menuTitle: {
+    position: "absolute",
+    top: "0",
+    left: 0,
     marginLeft: "2%",
     marginTop: "1%",
-    "& $h3": {
+    "& $h5": {
       marginTop: 0,
       color: "white",
+      fontSize: 20,
     },
   },
 }));
@@ -27,8 +45,9 @@ const MenuItem = (props) => {
 
   return (
     <div className={classes.menuContainer}>
+      <img src={props.image} alt="item_image" />
       <div className={classes.menuTitle}>
-        <h3>Item Name</h3>
+        <h5>{props.itemName}</h5>
       </div>
     </div>
   );
